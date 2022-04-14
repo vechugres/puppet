ac2f40a8c0e0d8602e00105a8d26f791015ae66a
Kirill Serguchev <101435762+vechugres@users.noreply.github.com>
2022-04-14 16:40:19 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,67 @@
-node slave2.puppet {
- include role::web_server
+node 'master.puppet' {
+
+include nginx
+
+nginx::resource::server { 'static':
+  listen_port => 80,
+  proxy => 'http://192.168.50.10:80',
+  }
+  
+nginx::resource::server { 'dynamic':
+  listen_port => 81,
+  proxy => 'http://192.168.50.15:80',
+  }
+  
+}
+
+node 'slave1.puppet' {
+
+ package { 'httpd':
+  ensure => latest,
+ }
+
+ file {'/var/www/html/index.html':
+  ensure => file,
+  source => 'puppet:///modules/static/index.html'
+ }
+
+ service { 'httpd':
+  ensure => running,
+  enable => true,
+ }
+
+ service { 'firewalld':
+  ensure => stopped,
+  enable => false,
+ }
+
+ file {'/root/README':
+  ensure => absent,
+ }
+}
+
+node 'slave2.puppet' {
+
+ package { ['httpd','php'] :
+  ensure => latest,
+ }
+
+ file {'/var/www/html/index.php':
+  ensure => file,
+  source => 'puppet:///modules/dynamic/index.php'
+ }
+ 
+ service { 'httpd':
+  ensure => running,
+  enable => true,
+ }
+ 
+ service { 'firewalld':
+  ensure => stopped,
+  enable => false,
+ }
+ 
+ file {'/root/README':
+  ensure => absent,
+ }
 }
