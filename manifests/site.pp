8cd6333b52c9da31aee6816ca1f04ddf21a7e7ea
Kirill Serguchev <101435762+vechugres@users.noreply.github.com>
2022-04-15 05:59:36 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,19 @@
+node 'slave1' {
+  include static
+  file {'/root/README':
+    ensure => absent
+    }
+}
+
+node 'slave2' {
+  include dynamic
+  file {'/root/README':
+    ensure => absent
+    }
+}
+
 node 'master.puppet' {
-
 include nginx
-
 nginx::resource::server { 'static':
   listen_port => 80,
   proxy => 'http://192.168.50.10:80',
@@ -14,64 +26,44 @@ nginx::resource::server { 'dynamic':
   
 }
 
-node 'slave1.puppet' {
-
- package { 'httpd':
-  ensure => latest,
- }
-
- file {'/var/www/html/index.html':
-  ensure => file,
-  source => 'puppet:///modules/static/index.html'
- }
- 
-file {'/etc/httpd/conf.d/static.conf':
-  ensure => file,
-  source => 'puppet:///modules/static/static.conf',
- }
-
- service { 'httpd':
-  ensure => running,
-  enable => true,
- }
-
- service { 'firewalld':
-  ensure => stopped,
-  enable => false,
- }
-
- file {'/root/README':
-  ensure => absent,
- }
+class dynamic{
+  package{['httpd', 'php']:
+    ensure => installed
+  }
+  file {'/var/www/html':
+    ensure => directory
+  }
+  file {'/var/www/html/index.php':
+    ensure => file,
+    source => 'puppet:///modules/dynamic/index.php'
+  }
+  file{'/etc/httpd/conf.d/dynamic.conf':
+    ensure => file,
+    source => 'puppet:///modules/dynamic/dynamic.conf',
+    notify => Service['httpd']
+  }
+  service{'httpd':
+    ensure => running
+  }
 }
 
-node 'slave2.puppet' {
-
- package { ['httpd','php'] :
-  ensure => latest,
- }
-
- file {'/var/www/html/index.php':
-  ensure => file,
-  source => 'puppet:///modules/dynamic/index.php'
- }
- 
-file {'/etc/httpd/conf.d/dynamic.conf':
-  ensure => file,
-  source => 'puppet:///modules/dynamic/dynamic.conf',
- }
- 
- service { 'httpd':
-  ensure => running,
-  enable => true,
- }
- 
- service { 'firewalld':
-  ensure => stopped,
-  enable => false,
- }
- 
- file {'/root/README':
-  ensure => absent,
- }
+class static{
+  package{['httpd', 'php']:
+    ensure => installed
+  }
+  file {'/var/www/html':
+    ensure => directory
+  }
+  file {'/var/www/html/index.php':
+    ensure => file,
+    source => 'puppet:///modules/dynamic/index.php'
+  }
+  file{'/etc/httpd/conf.d/static.conf':
+    ensure => file,
+    source => 'puppet:///modules/dynamic/static.conf',
+    notify => Service['httpd']
+  }
+  service{'httpd':
+    ensure => running
+  }
 }
