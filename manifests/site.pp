70fd42c192b38d96c0dd776e671700f40280fff6
Kirill Serguchev <101435762+vechugres@users.noreply.github.com>
2022-04-16 20:57:35 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -86,7 +86,7 @@ include wget
      source      => 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar',
      destination => '/opt/minecraft/',
      timeout     => 0,
-      verbose     => false,
+     verbose     => false,
 }
 
  file {'/etc/systemd/system/minecraft-server.service':
